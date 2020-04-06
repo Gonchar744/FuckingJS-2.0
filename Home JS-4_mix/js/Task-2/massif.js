7cfe3be23074f137893b55da1a2fafe3f7935dfe
Victor744 <60651507+Gonchar744@users.noreply.github.com>
2020-04-06 17:28:38 +0300

    Add files via upload

---
+++ Home JS-4_mix/js/Task-2/massif.js
@@ -0,0 +1,9 @@
+const massif = [23, 2, 45, 56, 1, 22, 134, 8, 33, 22, 44, 44, 3, 0, 5, 0, 56, 1, 1, 1, 70];
+
+const newMassif = massif.reduce((uniq, item) => {
+    return uniq.includes(item) ? uniq : [...uniq, item];
+}, [])
+
+console.log(newMassif);
+
+document.write(newMassif);

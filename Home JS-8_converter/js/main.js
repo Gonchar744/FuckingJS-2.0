b6bf81e29489aa8af44b3a456d1bbf8c59491d4d
Vadym Moiseyenko <vadym.moiseyenko@agiliway.com>
2020-05-17 01:56:45 +0300

    Code refactor

--- Home JS-8_converter/js/main.js
+++ Home JS-8_converter/js/main.js
@@ -1,19 +1,65 @@
-const button = document.getElementById('convert');
+(function(){
+    const button = document.getElementById('convert');
+    if(!button){
+        return;
+    }
 
-button.addEventListener('click', () => convertCurrency());
+    button.addEventListener('click', () => convertCurrency());
 
-function convertCurrency() {
-    button.disabled = true;
-    const from = document.getElementById('from').value;
-    const to = document.getElementById('to').value;
-    let ammount = document.getElementById('cash').value;
-    const url = `https://api.exchangeratesapi.io/latest?base=${from}`;
-    fetch(url)
-        .then(data => data.ok && data.json())
-        .then(({ rates }) => {
-            let result = ammount * rates[to];
-            document.getElementById('result').innerHTML = `<span class='result'> Результат конвертації:  ${result.toFixed(2)} </span>`;
-        }).catch(err => document.getElementById('result').innerHTML = err);
-    button.disabled = false;
-}
+    async function convertCurrency() {
+        async function fetchData(fromCurrency){
+            const url = `https://api.exchangeratesapi.io/latest?base=${fromCurrency}`;
 
+            try{
+                const data = await fetch(url);
+                if(data.ok){
+                    const { rates } = await data.json();
+                    return rates;
+                }
+                
+            } catch(e){
+                console.error("Some Errors when fetching data");
+            }
+                
+            return null;
+        }
+        
+        async function countResult(rates){            
+            toCurrency = toCurrencyElement.value;
+            amount = amountElement.value || 0;
+
+            if(!rates || !rates[toCurrency]){
+                return null;
+            }
+
+            return amount * rates[toCurrency];
+        }
+        
+        const resultElement = document.getElementById('result');
+        const fromCurrencyElement = document.getElementById('from');
+        const toCurrencyElement = document.getElementById('to');
+        const amountElement = document.getElementById('cash');
+
+        if(!resultElement || !fromCurrencyElement || !toCurrencyElement || !amountElement){
+            console.error("Some of the elements not found");
+            return null;
+        }
+        
+        button.disabled = true;
+        
+        try{
+            const ratesDataFromServer = await fetchData(fromCurrencyElement.value);
+            const result = await countResult(ratesDataFromServer)
+
+            if(typeof result === 'number'){
+                resultElement.innerHTML = `<span class='result'> Результат конвертації:  ${result.toFixed(2)} </span>`;
+            } else {
+                resultElement.innerHTML = `<span class='error'> Помилка під час розрахунку</span>`;;
+            }
+        } catch(e){
+            console.error("Some another issue :(");
+        }
+
+        button.disabled = false;
+    }
+})()
